a8f63e28d983480d5db8c68ef56ff98c4e315280
Théo Rousseau <48555250+zewcro@users.noreply.github.com>
2022-10-03 16:18:52 +0200

    Add Routes & Front updates

--- pizzamama/menu/views.py
+++ pizzamama/menu/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Pizza
 # Create your views here.
 def index(request):
-    return HttpResponse("Les Pizzas")
+    pizzas = Pizza.objects.all().order_by('prix')
+    return render(request, 'menu/index.html', {'pizzas': pizzas})
--- pizzamama/pizzamama/urls.py
+++ pizzamama/pizzamama/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('menu/', include('menu.urls')),
+    path('', include('main.urls')),
 ]
